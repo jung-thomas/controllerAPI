ec89d2a08efa92b0654160d085e162b0e959c5f3
I809764 <thomas.jung@sap.com>
2018-05-17 15:23:11 -0400

    Add Controller APIs for all service* actions

--- node/router/index.js
+++ node/router/index.js
@@ -14,4 +14,11 @@ module.exports = (app, server) => {
     app.use("/node/domains", require("./routes/domains")()); 
     app.use("/node/shared_domains", require("./routes/shared_domains")());       
     app.use("/node/trusted_certificates", require("./routes/trusted_certificates")());      
+    app.use("/node/service_brokers", require("./routes/service_brokers")());     
+    app.use("/node/service_plans", require("./routes/service_plans")());    
+    app.use("/node/service_instances", require("./routes/service_instances")());     
+    app.use("/node/service_bindings", require("./routes/service_bindings")());   
+    app.use("/node/services", require("./routes/services")());   
+    app.use("/node/service_keys", require("./routes/service_keys")());     
+    app.use("/node/user_provided_service_instances", require("./routes/user_provided_service_instances")());      
 };
